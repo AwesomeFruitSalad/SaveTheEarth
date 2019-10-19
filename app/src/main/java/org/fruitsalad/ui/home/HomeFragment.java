71df1f502418524e41a2455152b55853616e9211
abhijitramesh <abhijitramesh2k@gmail.com>
2019-10-20 03:23:08 +0530

    added profile

--- app/src/main/java/org/fruitsalad/ui/home/HomeFragment.java
+++ app/src/main/java/org/fruitsalad/ui/home/HomeFragment.java
@@ -1,18 +1,15 @@
 package org.fruitsalad.ui.home;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.mikhaellopez.circularimageview.CircularImageView;
 
@@ -34,6 +31,11 @@ public class HomeFragment extends Fragment {
     public void onResume() {
         super.onResume();
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        LevelProgressBar levelProgressBar = getActivity().findViewById(R.id.p1);
+        levelProgressBar.setProgressWithAnimation(5F);
+        CircularImageView circularImageView  = getActivity().findViewById(R.id.circularImageView);
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.vector_abhijit_1);
+        circularImageView.setImageBitmap(largeIcon);
     }
     @Override
     public void onStop() {
