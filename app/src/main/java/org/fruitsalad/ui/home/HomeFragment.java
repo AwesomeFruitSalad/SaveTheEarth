278a90bddae2b7010ba1b33fc53ba1b2b7ee012c
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-10-20 05:59:56 +0530

    Minor code refactoring :toilet:

--- app/src/main/java/org/fruitsalad/ui/home/HomeFragment.java
+++ app/src/main/java/org/fruitsalad/ui/home/HomeFragment.java
@@ -13,17 +13,13 @@ import androidx.fragment.app.Fragment;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.HeatDataEntry;
 import com.anychart.charts.HeatMap;
 import com.anychart.enums.SelectionMode;
 import com.anychart.graphics.vector.SolidFill;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 import org.fruitsalad.R;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.fruitsalad.utility.MockData;
 
 import ir.alirezaiyan.progressbar.LevelProgressBar;
 
@@ -36,14 +32,6 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
-    }
-
-
-    private class CustomHeatDataEntry extends HeatDataEntry {
-        CustomHeatDataEntry(String x, String y, Integer heat, String fill) {
-            super(x, y, heat);
-            setValue("fill", fill);
-        }
     }
 
     @Override
@@ -92,52 +80,7 @@ public class HomeFragment extends Fragment {
                         "           '<span style=\"color: #CECECE\">Consequence: </span>' + this.y;\n" +
                         "   }");
 
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new CustomHeatDataEntry("Jan", "Monday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Jan", "Tuesday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Jan", "Wednsday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Jan", "Monday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Jan", "Thursday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Feb", "Tuesday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Feb", "Wednsday", 0, "#90caf9"));
-        data.add(new CustomHeatDataEntry("Feb", "Thursday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Feb", "Wednsday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("Feb", "Wednsday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("Feb", "Tuesday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("March", "Tuesday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("March", "Tuesday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("March", "Monday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("March", "Friday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("April", "Thursday", 0, "#90caf9"));
-        data.add(new CustomHeatDataEntry("April", "Thursday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("April", "Monday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("May", "Tuesday", 2, "#43a047"));
-        data.add(new CustomHeatDataEntry("May", "Tuesday", 2, "#43a047"));
-        data.add(new CustomHeatDataEntry("May", "Friday", 0, "#90caf9"));
-        data.add(new CustomHeatDataEntry("May", "Friday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("June", "Friday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("June", "Major", 2, "#43a047"));
-        data.add(new CustomHeatDataEntry("June", "Tuesday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("June", "Extreme", 2, "#43a047"));
-        data.add(new CustomHeatDataEntry("July", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("July", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("July", "Saturday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("August", "Thursday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("August", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("August", "Saturday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("August", "Friday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Sep", "Thursday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Sep", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Sep", "Friday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Sep", "Thursday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Sep", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Oct", "Wednsday", 3, "#43a047"));
-        data.add(new CustomHeatDataEntry("Oct", "Friday", 1, "#43a047"));
-        data.add(new CustomHeatDataEntry("Oct", "Friday", 0, "#43a047"));
-        data.add(new CustomHeatDataEntry("Oct", "Sunday", 2, "#43a047"));
-        data.add(new CustomHeatDataEntry("Oct", "Saturday", 3, "#43a047"));
-
-        riskMap.data(data);
+        riskMap.data(MockData.getMockHeatMapData());
 
 
         anyChartView.setChart(riskMap);
